84f070ec2216f18cd5cb1ce2cef4d1041a065e55
Sameer <sameergautam1999@gmail.com>
2021-03-01 18:55:38 +0530

    waiting bar coded

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Coin from './components/Coin';
 function App() {
   const [coins, setCoins] = useState([])
   const [search, setSearch] = useState('')
-
+  const BASE_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    axios.get(BASE_URL)
     .then(res => {
       setCoins(res.data)
       console.log(res.data)
