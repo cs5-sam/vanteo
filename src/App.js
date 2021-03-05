74467e443fa122c54bd8250c70429728b2eb7164
Sameer <sameergautam1999@gmail.com>
2021-03-05 11:24:38 +0530

    setting up routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
 import Home from './Home';
 import Track from './Track';
+import CoinDetailPage from './components/CoinDetailPage';
+import CoinSummaryPage from './components/CoinSummaryPage';
 
 function App() {
     return (
@@ -19,7 +21,8 @@ function App() {
         </nav>
         <Switch>
             <Route path="/" exact component={Home}></Route>
-            <Route path="/track" exact component={Track}></Route>
+            <Route path="/track" exact component={CoinSummaryPage}></Route>
+            <Route path="/track/summary" exact component={CoinSummaryPage}></Route>
         </Switch>
         </>
     </Router>
---
+++ src/components/CoinSummaryPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CoinSummaryPage = () => {
+    return (
+        <div>
+            <h1>Coin Summary</h1>
+            <h1>Coin Summary</h1>
+        </div>
+    )
+}
+
+export default CoinSummaryPage
