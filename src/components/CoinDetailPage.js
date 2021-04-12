b77b1160a2fbdc76fcca27fd36a0e755c5a69f6a
Sameer <sameergautam1999@gmail.com>
2021-04-12 14:09:56 +0530

    history component created

--- src/components/CoinDetailPage.js
+++ src/components/CoinDetailPage.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useParams } from 'react-router'
+import HistoryChart from '../components/HistoryChart'
+import CoinData from '../components/CoinData'
+import coinGecko from '../apis/coinGecko'
 
 const CoinDetailPage = () => {
-    const {id} = useParams()
+    const {coin} = useParams()
+    useEffect(() => {
+        const fetchData = async() => {
+            const results = await coinGecko.get(`/coins/${coin}/market_chart/`,{
+                params:{
+                    vs_currency: "usd",
+                    days:"1",
+                },
+            });
+            console.log(results.data)
+        };
+        fetchData()
+    }, []);
+    const renderData = () => {
+        return (
+            <div className="coinlist">
+                <HistoryChart/>
+                <CoinData/>
+            </div>
+        )
+    }
     return (
-        <div>
-            
-        </div>
+        renderData()
     )
 }
 
