9c0b8c98276eeaa89f6020d79bed1f18f7a55e39
Sameer <sameergautam1999@gmail.com>
2021-04-19 09:21:02 +0530

    main commit

--- src/components/CoinDetailPage.js
+++ src/components/CoinDetailPage.js
@@ -7,11 +7,14 @@ import coinGecko from '../apis/coinGecko'
 const CoinDetailPage = () => {
     const {id} = useParams()
     const [coinData, setCoinData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
         const fetchData = async() => {
 
+            setIsLoading(true)
+
             // simultaneoulsy calling all the end points of API
-            const {day, week, year} = await Promise.all([coinGecko.get(`/coins/${id}/market_chart/`,{
+            const [day, week, year, detail] = await Promise.all([coinGecko.get(`/coins/${id}/market_chart/`,{
                 params:{
                     vs_currency: "usd",
                     days:"1",
@@ -28,18 +31,30 @@ const CoinDetailPage = () => {
                     vs_currency: "usd",
                     days:"365",
                 },
+            }),
+            coinGecko.get("/coins/markets",{
+                params:{
+                    vs_currency:"usd",
+                    ids: id
+                }
             })
         ])
             
             console.log(day)
-            setCoinData({day: day, week: week, year:year})
+            setCoinData({day: day.data.prices, week: week.data.prices, year:year.data.prices, detail: detail.data[0]})
+            setIsLoading(false)
         };
         fetchData()
     }, []);
     const renderData = () => {
+        if (isLoading){
+            return(
+                <div>Loading...</div>
+            )
+        }
         return (
             <div className="coinlist">
-                <HistoryChart/>
+                {/* <HistoryChart/> */}
                 <CoinData/>
             </div>
         )
--- src/components/HistoryChart.js
+++ src/components/HistoryChart.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
+// import {Bar} from "react-chartjs-2"
 
-const HistoryChart = () => {
+const HistoryChart = () => {    
+    const chartRef = useRef()
+    // useEffect(() => {
+    //     if (chartRef && chartRef.current){
+    //         <Bar height={400} width={600} data={{
+    //             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                
+    //         }}
+    //         options={{
+    //             maintainAspectRatio:false
+    //         }}/>
+    //     }
+    // })
     return (
         <div>
-            
+            {/* <canvas ref={chartRef} id="myChart" width={250} height={250}></canvas> */}
         </div>
     )
 }
